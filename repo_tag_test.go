8983773ac6fef49203e7ee8cdbfde3e118bc3421
Lunny Xiao <xiaolunwen@gmail.com>
2019-03-02 09:11:11 +0800

    fix bug when get tag id (#147)
    * fix bug when get tag id
    
    * fix wrong test
    
    * use GetTagCommitID on getTag but not git command

--- repo_tag_test.go
+++ repo_tag_test.go
@@ -20,6 +20,19 @@ func TestRepository_GetTags(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Len(t, tags, 1)
 	assert.EqualValues(t, "test", tags[0].Name)
-	assert.EqualValues(t, "3ad28a9149a2864384548f3d17ed7f38014c9e8a", tags[0].ID.String())
+	assert.EqualValues(t, "37991dec2c8e592043f47155ce4808d4580f9123", tags[0].ID.String())
 	assert.EqualValues(t, "commit", tags[0].Type)
+}
+
+func TestRepository_GetTag(t *testing.T) {
+	bareRepo1Path := filepath.Join(testReposDir, "repo1")
+	bareRepo1, err := OpenRepository(bareRepo1Path)
+	assert.NoError(t, err)
+
+	tag, err := bareRepo1.GetTag("test")
+	assert.NoError(t, err)
+	assert.NotNil(t, tag)
+	assert.EqualValues(t, "test", tag.Name)
+	assert.EqualValues(t, "37991dec2c8e592043f47155ce4808d4580f9123", tag.ID.String())
+	assert.EqualValues(t, "commit", tag.Type)
 }
